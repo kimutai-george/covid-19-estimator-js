6dc085ae7da8b419369403655669deb5bcce7aa7
George Kimutai <georgekimuu#gmail.com>
2020-04-12 20:03:54 +0300

    estimator-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,205 +1,194 @@
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
 /**
  *  Convert to data - in the case where periodType = weeks & months
- * @param {*} data 
+ * @param {*} data
  */
 
 const normalizetimetoElapse = (data) => {
+  let timetoElapse = data.periodType;
 
-    const period_type = data.periodType;
-    const time_to_Elapse;
+  if (data.periodType === 'weeks') {
+    timetoElapse = data.timeToElapse * 7;
+  } else if (data.periodType === 'months');
+  // eslint-disable-next-line no-lone-blocks
+  {
+    timetoElapse = data.timeToElapse * 30;
+  }
 
-    if(period_type === "weeks")
-    {
-        time_to_Elapse = data.timeToElapse *= 7;
-    }
-    else if (period_type == "months")
-    {
-        time_to_Elapse = data.timeToElapse *= 30
-    }
-    else
-    {
-        time_to_Elapse = data.timeToElapse;
-    }
-
-    return { time_to_Elapse }
-}
+  return timetoElapse;
+};
 
 
 /**
  * estimate multiplier values
- * @param {*} data 
+ * @param {*} data
  */
 
 const constValues = (data) => {
-    const days = normalizetimetoElapse.time_to_Elapse;
+  const days = normalizetimetoElapse.time_to_Elapse;
 
-    const factor = Math.floor(days / 3)
+  const factor = Math.floor(days / 3);
 
-    const multiply_value = 2 ** factor
+  const multiplyValue = 2 ** factor;
 
-    const available_beds = (data.totalHospitalBeds * 0.35)
-
-
+  const availableBeds = (data.totalHospitalBeds * 0.35);
 
 
-    return { multiply_value, available_beds }
-}
+  return { multiplyValue, availableBeds };
+};
 
 
 /**
  * function to calculate Impact
- * @param {*} data 
+ * @param {*} data
  */
 const estimateInfectedCases = (data) => {
-    currentlyInfected = (data.reportedCases * 10);
-    projectedInfected = (data.reportedCases * 50);
+  const currentlyInfected = (data.reportedCases * 10);
+  const projectedInfected = (data.reportedCases * 50);
 
-    infectionsByCurrentTime = currentlyInfected * constValues.multiply_value
-    infectionsByProjectedTime = currentlyInfected * constValues.multiply_value
+  const infectionsByCurrentTime = currentlyInfected * constValues.multiply_value;
+  const infectionsByProjectedTime = currentlyInfected * constValues.multiply_value;
 
-    
-    return  { currentlyInfected, projectedInfected, infectionsByCurrentTime, infectionsByProjectedTime } 
-}
+
+  return {
+    currentlyInfected, projectedInfected, infectionsByCurrentTime, infectionsByProjectedTime
+  };
+};
 
 
 /**
  *  estimate Impact & severe cases
- * @param {*} data 
+ * @param {*} data
  */
 const estimateImpactCases = () => {
-    impactByRequestedTime = Math.floor(0.15 * estimateImpactCases.infectionsByCurrentTime)
-    severeImpactByRequestedTime = Math.floor(0.15 * estimateImpactCases.infectionsByProjectedTime)
+  const impactRequestedTime = Math.floor(0.15 * estimateImpactCases.infectionsByCurrentTime);
+  const severeRequestedTime = Math.floor(0.15 * estimateImpactCases.infectionsByProjectedTime);
 
 
-    return { impactByCurrentTime, severeCasesByRequestedTime }
-}
+  return { impactRequestedTime, severeRequestedTime };
+};
 
- /**
+/**
   * Hospital beds by requested time - Current case
-  * @param {*} data 
+  * @param {*} data
   */
 
-  const hospitalbedsBeds = () => {
-      hospitalbedsbyCurrentTime = Math.floor(constValues.available_beds * estimateImpactCases.impactByCurrentTime)
-      hospitalbedsbyProjectedTime = Math.floor(constValues.available_beds * estimateImpactCases.severeCasesByRequestedTime)
+const hospitalbedsBeds = () => {
+  const hospbedsCurrentTime = Math.floor(constValues.availableBeds * estimateImpactCases.impactRequestedTime);
+  const hospbedsProjectedTime = Math.floor(constValues.availableBeds * estimateImpactCases.severeRequestedTime);
 
 
-      return { hospitalbedsbyCurrentTime, hospitalbedsbyProjectedTime }
-  }
+  return { hospbedsCurrentTime, hospbedsProjectedTime };
+};
 
 
 /**
  *  Estimated cases for ICU by requested time - Current cases
- * @param {*} data 
+ * @param {*} data
  */
 
- const estimatedICUCases = () => {
-    casesForICUbyCurrentTime = Math.floor(0.05 * estimateInfectedCases.infectionsByCurrentTime)
-    casesForICUbyProjectedTime = Math.floor(0.05 * estimateInfectedCases.infectionsByProjectedTime)
+const estimatedICUCases = () => {
+  const casesForICUbyCurrentTime = Math.floor(0.05 * estimateInfectedCases.infectionsByCurrentTime);
+  const casesForICUbyProjectedTime = Math.floor(0.05 * estimateInfectedCases.infectionsByProjectedTime);
 
 
-     return { casesForICUbyCurrentTime, casesForICUbyProjectedTime };
- }
+  return { casesForICUbyCurrentTime, casesForICUbyProjectedTime };
+};
 
 
 /**
- *  Estimated cases for ventilation 
- * @param {*} data 
+ *  Estimated cases for ventilation
+ * @param {*} data
  */
 
- const estimatedVentilatorCases = () => {
-    casesForVentilatorsByCurrentime = Math.floor(0.02 * estimateInfectedCases.infectionsByCurrentTime)
-    casesForVentilatorsByProjectedTime = Math.floor(0.02 * estimateInfectedCases.infectionsByProjectedTime)
+const estimatedVentilatorCases = () => {
+  const casesForVentilatorsByCurrentime = Math.floor(0.02 * estimateInfectedCases.infectionsByCurrentTime);
+  const casesForVentilatorsByProjectedTime = Math.floor(0.02 * estimateInfectedCases.infectionsByProjectedTime);
 
 
-    return { casesForVentilatorsByCurrentime, casesForVentilatorsByProjectedTime }
- }
+  return { casesForVentilatorsByCurrentime, casesForVentilatorsByProjectedTime };
+};
 
 
- /**
+/**
   *   Estimate Economic Loses
-  * @param {*} data 
+  * @param {*} data
   */
 
-  const estimatedEconomicLoses = () => {
+const estimatedEconomicLoses = () => {
+  const dollarsinFlightCurrentSituation = Math.floor((estimateInfectedCases.infectionsByCurrentTime * 0.65 * 1.5) / 30);
+  const dollarsinFlightProjectedSituation = Math.floor((estimateInfectedCases.infectionsByProjectedTime * 0.65 * 1.5) / 30);
 
-    dollarsinFlightCurrentSituation = Math.floor((estimateInfectedCases.infectionsByCurrentTime * 0.65 * 1.5) /30)
-    dollarsinFlightProjectedSituation = Math.floor((estimateInfectedCases.infectionsByProjectedTime * 0.65 * 1.5) /30)
 
-
-    return { dollarsinFlightCurrentSituation, dollarsinFlightProjectedSituation }
-
-  }
+  return { dollarsinFlightCurrentSituation, dollarsinFlightProjectedSituation };
+};
 
 /**
  *  Results for project i.e both current situation and projected outcome.
- * @param {*} data 
+ * @param {*} data
  */
 
 const covid19Predictor = (data) => {
-    const impact = {
-        currentlyInfected: estimateInfectedCases.currentlyInfected,
-        infectionsByRequestedTime: estimateInfectedCases.infectionsByCurrentTime,
-        severeCasesByRequestedTime: estimateImpactCases.impactByCurrentTime,
-        hospitalBedsByRequestedTime: hospitalbedsBeds.hospitalbedsCurrentImpacts,
-        casesForICUBYRequestedTime: estimatedICUCases.casesForICUbyCurrentTime,
-        casesForVentilatorsByRequestedTime: estimatedVentilatorCases.casesForVentilatorsByCurrentime,
-        dollarsInFlight: estimatedEconomicLoses.dollarsinFlightCurrentSituation
-    };
+  const impact = {
+    currentlyInfected: estimateInfectedCases.currentlyInfected,
+    infectionsByRequestedTime: estimateInfectedCases.infectionsByCurrentTime,
+    severeCasesByRequestedTime: estimateImpactCases.impactByCurrentTime,
+    hospitalBedsByRequestedTime: hospitalbedsBeds.hospitalbedsCurrentImpacts,
+    casesForICUBYRequestedTime: estimatedICUCases.casesForICUbyCurrentTime,
+    casesForVentilatorsByRequestedTime: estimatedVentilatorCases.casesForVentilatorsByCurrentime,
+    dollarsInFlight: estimatedEconomicLoses.dollarsinFlightCurrentSituation
+  };
 
-    const severeImpact = {
-        currentlyInfected: estimateInfectedCases.projectedInfected,
-        infectionsByRequestedTime: estimateInfectedCases.infectionsByProjectedTime,
-        severeCasesByRequestedTime: estimateImpactCases.severeImpactByRequestedTime,
-        hospitalBedsByRequestedTime: hospitalbedsBeds.hospitalbedsbyProjectedTime,
-        casesForICUBYRequestedTime: estimatedICUCases.casesForICUbyProjectedTime,
-        casesForVentilatorsByRequestedTime: estimatedVentilatorCases.casesForVentilatorsByProjectedTime,
-        dollarsInFlight: estimatedEconomicLoses.dollarsinFlightProjectedSituation
-    }
+  const severeImpact = {
+    currentlyInfected: estimateInfectedCases.projectedInfected,
+    infectionsByRequestedTime: estimateInfectedCases.infectionsByProjectedTime,
+    severeCasesByRequestedTime: estimateImpactCases.severeImpactByRequestedTime,
+    hospitalBedsByRequestedTime: hospitalbedsBeds.hospitalbedsbyProjectedTime,
+    casesForICUBYRequestedTime: estimatedICUCases.casesForICUbyProjectedTime,
+    casesForVentilatorsByRequestedTime: estimatedVentilatorCases.casesForVentilatorsByProjectedTime,
+    dollarsInFlight: estimatedEconomicLoses.dollarsinFlightProjectedSituation
+  };
 
-    return { data, impact, severeImpact }
-}
-
+  return { data, impact, severeImpact };
+};
 
 
 const covid19ImpactEstimator = (data) => {
+  // eslint-disable-next-line no-undef
+  const estimator = chain(
 
-    const estimator = chain(
+    // challenge 1
+    estimateinfectedImpact,
+    estimateinfectedsevereImpact,
 
-        // challenge 1 
-        estimateinfectedImpact,
-        estimateinfectedsevereImpact,
+    // challenge -2
+    estimateImpactCases,
+    estimatesevereImpactCases,
+    hospitalbedsCurrentImpacts,
+    hospitalbedsProjectedCases,
 
-        //challenge -2 
-        estimateImpactCases,
-        estimatesevereImpactCases,
-        hospitalbedsCurrentImpacts,
-        hospitalbedsProjectedCases,
+    // challenge 3
 
-        //challenge 3
+    estimatedICUCurrentcases,
+    estimatedICUProjectedCases,
+    estimatedVentilatorCurrentCases,
+    estimatedVentilatorProjectesCases,
+    estimatedLosesCurrentSituaton,
+    estimatedLosesProjectedSituaton,
 
-        estimatedICUCurrentcases,
-        estimatedICUProjectedCases,
-        estimatedVentilatorCurrentCases,
-        estimatedVentilatorProjectesCases,
-        estimatedLosesCurrentSituaton,
-        estimatedLosesProjectedSituaton,
+    // Final Projection
 
-        // Final Projection
+    covid19Predictor
 
-        covid19Predictor
+  );
 
-    );
-
-    return estimator({
-        data,
-        impact: {},
-        severeImpact: {}
-    })
-
-    
-
-}
+  return estimator({
+    data,
+    impact: {},
+    severeImpact: {}
+  });
+};
 
 
 export default covid19ImpactEstimator;
