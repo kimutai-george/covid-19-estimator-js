360a0af32f6b26b2fa241b35f3092046a27b4289
George Kimutai <georgekimuu#gmail.com>
2020-04-14 08:19:14 +0300

    api-6

--- src/routes/Covid19Route.js
+++ src/routes/Covid19Route.js
@@ -9,7 +9,6 @@ const covid19Ctrl = require('../controllers/Covid19Controller');
 route.post('/', covid19Ctrl.postCovid19Data); // post Covid data
 route.post('/xml', covid19Ctrl.postCovid19DataXml); // post Covid data
 route.get('/logs', covid19Ctrl.getLogs); // get logs
-route.get('/', covid19Ctrl.getIndex); // get logs
 
 
 module.exports = route;
